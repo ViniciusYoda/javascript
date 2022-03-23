cb2aeec740e8be6a3a13fefdb8307a70dc5df070
ViníciusYoda <rm92969@fiap.com.br>
2022-03-23 19:05:58 -0300

    apredendo funções

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.7
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO! `)
+}
+console.log(`diriga sempre usando cinto de segurança`)
