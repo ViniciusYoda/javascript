0695a8e4de183d18dc4d19bd0f17be7d4a69e922
ViníciusYoda <rm92969@fiap.com.br>
2022-03-26 19:36:30 -0300

    Criando um site que fala as horas

--- desafios/des02/pag.js
+++ desafios/des02/pag.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.scr = 'img/Dia.png.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'img/noite.png'
+        document.body.style.background = '#515154'
+    }
+}
