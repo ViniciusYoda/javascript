83c47eae8963a3d9b84f0783cb781a034a527001
ViníciusYoda <rm92969@fiap.com.br>
2022-03-30 18:32:32 -0300

    iniciando o desafio

---
+++ desafios/des06/Code.js
@@ -0,0 +1,28 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista') 
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value)) {
+        
+    } else {
+        window.alert('Valor invalido ou já encontrado na lista')
+    }
+}
