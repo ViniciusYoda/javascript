b7729b5dc3fa4586a775467f44350cde14b3b711
ViníciusYoda <rm92969@fiap.com.br>
2022-04-03 15:55:22 -0300

    Nome e idade perguntado

---
+++ desafios/des08/código.js
@@ -0,0 +1,7 @@
+
+
+function botão() {
+   let nome = window.prompt('Qual é seu nome?')
+   let idade = window.prompt('Qual é à sua idade?')
+   window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos`)
+}
