77de09303b6bfbff22936ef1f84bc00844cf6d50
ViníciusYoda <rm92969@fiap.com.br>
2022-04-03 15:36:49 -0300

    mensagem entregue

---
+++ desafios/des07/código.js
@@ -0,0 +1,9 @@
+function click1() {
+    window.alert('Você clicou no primeiro botão')
+}
+function click2() {
+    window.alert('Você clicou no segundo botão')
+}
+function click3() {
+    window.alert('Você clicou no terceiro botão')
+}
