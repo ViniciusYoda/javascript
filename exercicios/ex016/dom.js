02cacc00ece22185c1c1c11ec20a16ff3e1f7604
ViníciusYoda <rm92969@fiap.com.br>
2022-05-18 17:08:49 -0300

    DOM

---
+++ exercicios/ex016/dom.js
@@ -0,0 +1,10 @@
+document.title = "JavaScript - DOM";
+
+
+//mundadno background
+document.body.style.background = "#cccccc";
+//document.body.style.backgroundImage = "url('img/tijolo.jpg')";
+
+//escrever na página
+document.write("<h1>Escrevendo um título</h1>")
+document.write("<p>Escrevendo um parágrafo</p><br>pulando uma linha")
