4b06fd28f09ea398c9218021ba5a2d70077fefd7
ViníciusYoda <rm92969@fiap.com.br>
2022-06-13 14:52:15 -0300

    js

---
+++ exercicios/ex017/js/main.js
@@ -0,0 +1,22 @@
+const firstName = "Vinícius"
+const lastName = "Yoda"
+
+const names = 'Antonio'
+
+console.log("Meu nome é " + firstName + " " + lastName)
+console.log(`Meu nome é $(firstName.toUpperCase()) $(lastName.toLowerCase())`);
+
+console.log(names.split(","))
+
+const number = 5;
+
+console.log(number.toString())
+console.log(typeof number.toString())
+
+console.log(2 == 2);
+
+const x = null;
+
+const y = undefined;
+
+
