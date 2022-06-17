d0f2a163dc28f27f283adb5c58339929eb7c03d2
ViníciusYoda <rm92969@fiap.com.br>
2022-06-17 17:52:24 -0300

    media deu certo

---
+++ exercicios/ex019/media.js
@@ -0,0 +1,9 @@
+function media() {
+    var tn1 = window.document.getElementById('txtn1')
+    var tn2 = window.document.querySelector('input#txtn2')
+    var res = window.document.getElementById('res')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var s = (n1 + n2) / 2
+    res.innerText = `A média entre ${n1} e ${n2} é ${s}`
+}
