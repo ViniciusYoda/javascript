4c579ff016294f9caac4eb983b7978f9ea4bd11c
ViníciusYoda <rm92969@fiap.com.br>
2022-06-18 17:56:03 -0300

    funçao

---
+++ exercicios/ex017/js/funcoes.js
@@ -0,0 +1,15 @@
+function sum(a,b = 10){
+    return a + b
+}
+
+const sumArrow = (a, b) =>{
+    return a + b;
+}
+
+const sumValue = sum(2)
+
+let value = sum(2, 3)
+
+console.log(sumValue)
+
+console.log(value)
