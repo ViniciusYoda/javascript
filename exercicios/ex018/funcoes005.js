883043740b3f588affdc3d87e8112e03335fc5fc
ViníciusYoda <rm92969@fiap.com.br>
2022-06-20 14:56:51 -0300

    retornando funçoes

---
+++ exercicios/ex018/funcoes005.js
@@ -0,0 +1,4 @@
+var myText = 'I am a string';
+var newString = myText.replace('string', 'sausage');
+console.log(newString);
+// the replace() string function takes a string, replaces one substring with another, and returns a new string witg the replacement made
