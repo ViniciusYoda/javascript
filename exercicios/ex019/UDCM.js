b0efa1771778fb0f44a46fbf42e0af7db74b533f
ViníciusYoda <rm92969@fiap.com.br>
2022-06-23 13:41:54 -0300

    a

--- exercicios/ex019/UDCM.js
+++ exercicios/ex019/UDCM.js
@@ -1,10 +1,10 @@
 function ver(){
     const res = document.getElementById('res')
     const num = document.getElementById('num')
-    let u = num / 1 % 10
-    let d = num / 10 % 10
-    let c = num / 100 % 10
-    let m = num / 1000 % 10
+    let u = num % 1 % 10
+    let d = num % 10 % 10
+    let c = num % 100 % 10
+    let m = num % 1000 % 10
     res.innerText = `Unidade: ${u}
                      Dezena:  ${d}
                      Centena: ${c}
