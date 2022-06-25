dd6d7166f80ba432f4d7a5ac92f0673b39ad1384
ViníciusYoda <rm92969@fiap.com.br>
2022-06-25 13:29:00 -0300

    objeto

---
+++ exercicios/ex018/oobj002.js
@@ -0,0 +1,8 @@
+walsh = new Professor('Walsh', 'Psychology')
+lillian = new Professor('Lillian', 'Poetry')
+
+walsh.teaches // 'Psychology'
+walsh.introduceSelf() // 'My name is Professor Walsh and I will be your Psychology professor.'
+
+lillian.teaches // 'Poetry'
+lillian.introduceSelf() // 'My name is Professor Lillian and I will be your Poetry professor.'
