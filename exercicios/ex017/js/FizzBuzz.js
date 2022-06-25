8624af1f4ede23cbd07a6d48b5da75455875eeb6
ViníciusYoda <rm92969@fiap.com.br>
2022-06-25 17:45:46 -0300

    FizzBuzz

---
+++ exercicios/ex017/js/FizzBuzz.js
@@ -0,0 +1,14 @@
+const resultado = fizzBuzz(15);
+console.log(resultado);
+
+function fizzBuzz(entrada){
+    if ( typeof entrada !== 'number')
+        return 'Não é um número';
+    if (entrada % 3 === 0 && entrada % 5 === 0)
+        return 'FizzBuzz'
+    if (entrada % 3 === 0)
+        return 'Fizz'
+    if (entrada % 5 === 0)
+        return 'Buzz'
+    return entrada;
+}
