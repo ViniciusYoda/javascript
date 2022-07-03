c5bc64ec1e08b8e2c8acc1cfafc94f333ccc6e82
ViníciusYoda <rm92969@fiap.com.br>
2022-07-03 13:53:13 -0300

    astericos

---
+++ exercicios/ex017/js/asteriscos.js
@@ -0,0 +1,16 @@
+exibirAstericos(25);
+
+function exibirAstericos(linhas){
+    let padrao = ''
+    for(let linha = 1; linha <= linhas; linha++){
+        padrao += '*'
+        console.log(padrao)
+    }
+    for(let linha = 1; linha <= linhas; linha++){
+        let padrao = ''
+        for(let i = 0; i < linha; i++){
+            padrao += '*'
+        }
+        console.log(padrao)
+    }
+}
