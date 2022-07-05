f2a98a947cf9783dc45428804af77ba1b27849b1
ViníciusYoda <rm92969@fiap.com.br>
2022-07-05 17:47:37 -0300

    DOM

---
+++ exercicios/ex020/dom11.js
@@ -0,0 +1,12 @@
+var form = document.querySelector('form');
+var fname = document.getElementById('fname');
+var lname = document.getElementById('lname');
+var submit = document.getElementById('submit');
+var para = document.querySelector('p');
+
+form.onsubmit = function(e) {
+    if (fname.value === '' || lname.value === '') {
+        e.preventDefault();
+        para.textContent = 'You need to fill in both names!'
+    }
+}
