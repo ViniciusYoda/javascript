3bd41c303c5d5ff680470a3b5ac37833b1b7e00d
ViníciusYoda <rm92969@fiap.com.br>
2022-07-08 16:20:07 -0300

    String

---
+++ exercicios/ex017/js/String.js
@@ -0,0 +1,13 @@
+// Tipo primitivo
+const mensagem = 'minha primeira mensagem'
+// tipo objeto
+const outraMensagem = new String('bom dia')
+console.log(outraMensagem.length)
+console.log(outraMensagem[2])
+console.log(mensagem.includes('primeira'))
+console.log(mensagem.startsWith('verde'))
+console.log(mensagem.endsWith('mensagem'))
+console.log(mensagem.indexOf('primeira'))
+console.log(mensagem.replace('minha', 'sua'))
+console.log(mensagem.trim())
+console.log(mensagem.split(' '))
