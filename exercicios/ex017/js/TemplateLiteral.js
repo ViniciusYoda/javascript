ef8e5ef85595fb89648c9b329ad6ef323c96876e
ViníciusYoda <rm92969@fiap.com.br>
2022-07-09 11:30:43 -0300

    Template literal

---
+++ exercicios/ex017/js/TemplateLiteral.js
@@ -0,0 +1,15 @@
+const mensagem = 'Oi isso é a minha \n \'primeira\' mensagem';
+const outra = `oi isso é a minha primeira
+mensagem`;
+const nome = 'rafael';
+
+const email =
+`Olá ${nome}
+
+Obrigado
+
+Por
+
+Estar 
+
+Aqui`
