bdba655e51602f6250d53834bc2f745927df70c6
ViníciusYoda <rm92969@fiap.com.br>
2022-07-10 11:17:49 -0300

    date

---
+++ exercicios/ex017/js/date.js
@@ -0,0 +1,8 @@
+// Date
+const data1 = new Date();
+const date2 = new Date('March 06 2019 09:30');
+console.log(date2)
+const date3 = new Date(2019,02,06,9,30)
+console.log(date3)
+date3.setFullYear(2030)
+console.log(date3)
