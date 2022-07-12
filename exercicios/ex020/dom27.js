f65e322d93037c6528fb8a3c395a84722bcc922d
ViníciusYoda <rm92969@fiap.com.br>
2022-07-12 14:39:31 -0300

    Comment

---
+++ exercicios/ex020/dom27.js
@@ -0,0 +1,15 @@
+//Comment
+
+// A interface Comentário representa notações textuais dentro da marcação; embora geralmente não seja mostrado visualmente, esses comentários estão disponíveis para serem lidos na visualização de origem
+
+// Os comentários são representados em HTML e XML como conteúdo entre '<!--' e '-->'. Em XML, como na marcação SVG ou MathML, a sequência de caracteres '--' não pode ser usada em um comentário.
+
+// Properties
+// Essa interface não possui propriedade específica, mas herda as de seu pai, CharacterData, e indiretamente as de Node.
+
+// Constructor
+// Comment()
+//Retorna um novo objeto Comment com o parâmetro como seu conteúdo textual. Se não estiver presente, seu valor padrão é a string vazia, ''.
+
+//Methods
+// Essa interface não possui um método específico, mas herda os de seu pai, CharacterData, e indiretamente os de Node.
