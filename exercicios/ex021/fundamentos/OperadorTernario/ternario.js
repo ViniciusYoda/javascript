9ff0a1d34f7a09f0213eb6e48a084a75115cb4fb
ViníciusYoda <rm92969@fiap.com.br>
2022-07-20 16:28:32 -0300

    Operador ternario

---
+++ exercicios/ex021/fundamentos/OperadorTernario/ternario.js
@@ -0,0 +1,5 @@
+let idade = 18
+
+let resposta = (idade < 3) ? 'Ainda um bebê' : (idade < 12) ? 'Ainda uma criança' : (idade < 18) ? 'Ainda um adolescente' : (idade < 60) ? 'Um grande adulto' : 'Um bom velinho'
+
+console.log(resposta);
