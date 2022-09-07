479535a3e3e0fad9fc7056556a5cb75cc07e7b9f
ViníciusYoda <rm92969@fiap.com.br>
2022-09-07 14:20:57 -0300

    array

---
+++ exercicios/ex021/DataTypes/Arrays.js
@@ -0,0 +1,11 @@
+/*
+
+Matrizes
+Os objetos permitem que você armazene coleções de valores com chave. Isso é bom.
+
+Mas muitas vezes descobrimos que precisamos de uma coleção ordenada , onde temos um 1º, um 2º, um 3º elemento e assim por diante. Por exemplo, precisamos disso para armazenar uma lista de algo: usuários, mercadorias, elementos HTML etc.
+
+Não é conveniente usar um objeto aqui, porque ele não fornece métodos para gerenciar a ordem dos elementos. Não podemos inserir uma nova propriedade “entre” as já existentes. Objetos simplesmente não são feitos para tal uso.
+
+Existe uma estrutura de dados especial chamada Array, para armazenar coleções ordenadas.
+
