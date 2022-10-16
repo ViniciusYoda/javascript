46965cc36c7afe4bbeb011c1728e917df892bb56
Vinícius Yoda <vyoda4604@gmail.com>
2022-10-16 13:47:47 -0300

    Map and set

---
+++ exercicios/ex021/DataTypes/Filtrar anagramas.js
@@ -0,0 +1,30 @@
+function aclean(arr) {
+    let map = new Map();
+
+    for (let word of arr) {
+        // split the word by letters, sort them and join back
+        let sorted = word.toLowerCase().split('').sort().join(''); // (*)
+        map.set(sorted, word)
+    }
+
+    return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+console.log( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era"
+
+function aclean2(arr2) {
+    let obj = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        let sorted = arr[i].toLowerCase().split("").sort().join("");
+        obj[sorted] = arr[i];
+    }
+
+    return Object.values(obj);
+} 
+
+let arr2 = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+console.log( aclean(arr2) );
