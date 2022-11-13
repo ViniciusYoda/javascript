c7e1445bbe7abed35c52267cd39dfa7ab4e1c26d
Vinícius Yoda <vyoda4604@gmail.com>
2022-11-13 13:27:11 -0300

    exercicios

---
+++ exercicios/ex021/DataTypes/Armazenar Sinalizadores.js
@@ -0,0 +1,22 @@
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"},
+];
+
+let readMessages = new WeakSet();
+
+// two messages have been read
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+// readMessages has 2 elements
+
+// ...let´s read the first message again!
+readMessages.add(messages[0]);
+// readMessages still has 2 unique elements
+
+// answer: was the message[0] read?
+console.log("Read message 0: "  + readMessages.has([0]));
+
+messages.shift();
+// now readMessages has 1 element (technically memory may be cleaned later)
