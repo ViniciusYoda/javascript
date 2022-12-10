d78cb8d658e064305f72365200491673b304d43d
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-10 14:14:25 -0300

    tasks

---
+++ exercicios/ex021/6 - Advanced working with functions/6.6 - Function object, NFE/Definir e diminuir para contador.js
@@ -0,0 +1,15 @@
+function makeCounter() {
+   let count = 0;
+
+   function counter() {
+      return count++;
+   }
+
+   counter.set = value => count = value;
+
+   counter.decrease = () => count--;
+
+   return counter;
+}
+
+makeCounter()
