83d1c244ecfae493b7ce51ecc74b8094da419701
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-23 16:25:11 -0300

    prototype

---
+++ javascript/8 - Prototypes, inheritance/8.2 -F.prototypes/prototype.js
@@ -0,0 +1,25 @@
+const Nave = function(energia) {
+   this.energia = energia
+   this.disparos=100
+}
+
+const n1 = new Nave(100)
+
+Nave.prototype.vidas = 3
+Nave.prototype.disparar = function() {
+   if (this.disparos > 0) {
+      this.disparos--
+   } 
+}
+
+n1.disparar()
+n1.disparar()
+n1.disparar()
+n1.disparar()
+n1.disparar()
+
+console.log(Nave)
+console.log(n1)
+console.log(n1.energia)
+console.log(n1.vidas)
+console.log(n1.disparos)
