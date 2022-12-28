d1f29f68748fe421d82116a120fbf5d0e9b27948
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-28 16:49:01 -0300

    static

---
+++ javascript/9 - Classes/9.3 - Static properties and methods/static.js
@@ -0,0 +1,15 @@
+class Calculadora {
+
+   static somar(a, b) {
+      console.log(a + b)
+   }
+
+   static subtrair(a, b) {
+      console.log(a - b)
+   }
+}
+
+Calculadora.somar(10, 5)
+
+Calculadora.subtrair(8, 4)
+
