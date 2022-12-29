60a45ea63801627c6b881f85b663bca06261c1a9
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-29 16:50:39 -0300

    tasks

---
+++ javascript/10 - Error handling/10.2 - Custom erros, extending Error/Herdar de SyntaxError.js
@@ -0,0 +1,14 @@
+class FormatError extends SyntaxError {
+   constructor(message) {
+      super(message);
+      this.name = this.constructor.name;
+   }
+}
+
+let err = new FormatError("formatting error");
+
+console.log( err.message );
+console.log( err.name );
+console.log( err.stack );
+
+console.log( err instanceof SyntaxError );
